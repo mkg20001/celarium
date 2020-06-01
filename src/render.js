6fe258178d19305e1938b233249a8481799f5de8
Maciej Krüger <mkg20001@gmail.com>
2020-06-01 16:43:58 +0200

    misc: cleanup

--- src/render.js
+++ src/render.js
@@ -5,8 +5,8 @@ const path = require('path')
 const fs = require('fs')
 
 module.exports = (files, outFolder) => {
-  for (const file in files) {
+  for (const file in files) { // eslint-disable-line guard-for-in
     const outName = path.join(outFolder, `${file}.js`)
-    fs.writeFileSync(outName, S(files[file]))    
+    fs.writeFileSync(outName, S(files[file]))
   }
 }
