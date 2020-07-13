1716da675745a2add9306f983c1bea1a44719d1f
Maciej Krüger <mkg20001@gmail.com>
2020-07-13 13:30:45 +0200

    feat: base render

---
+++ src/render/base.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const { L, S, Joi, iterateKeys } = require('../utils')
+
+module.exports = (config) => {
+  return L(`'use strict'
+
+const { configValidate } = require('celarium/src/include')
+
+module.exports = async (config) => {
+  const { value, error } = configValidate.validate(config)
+
+  if (error) {
+    throw error
+  }
+
+  config = value
+
+  const DBM = await (require('./db')(config.db))
+  const API = await (require('./api')(config.api, DBM))
+
+  return {
+    DBM,
+    API,
+    async start () {
+      await DBM.connect()
+      await API.start()
+    },
+    async stop () {
+      await DBM.disconnect()
+      await API.stop()
+    }
+  }
+}`)
+}
