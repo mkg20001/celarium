e875e355df11b7e87f3d8723b0dfc34c27c5b0a2
Maciej Krüger <mkg20001@gmail.com>
2020-07-30 16:15:09 +0200

    fix: add missing file

---
+++ src/render/acl.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { L, S, iterateKeys } = require('../utils')
+
+module.exports = models => {
+  const acls = iterateKeys(models, (modelName, model) => {
+    return {
+      lists: iterateKeys(model.acl, (listName, list) => {
+        return list
+      }),
+      base: {
+        read: model.read
+      },
+      attrs: iterateKeys(model.attributes, (attrName, attr) => {
+        if (attr.isList) {
+          return { append: attr.append, delete: attr.delete, read: attr.read }
+        }
+
+        return { modify: attr.modify, read: attr.read }
+      })
+    }
+  })
+
+  return L(`'use strict'
+
+module.exports = DBM => {
+  return (require('celarium/src/acl/db')(DBM, ${S(acls)}))
+}`)
+}
