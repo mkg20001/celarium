2c640a9cc005463a4d04c1cd3b867a28b76117fa
Maciej Krüger <mkg20001@gmail.com>
2020-08-28 16:29:10 +0200

    misc: cleanup further

--- src/render/_client.js
+++ src/render/_client.js
@@ -1,7 +1,7 @@
 'use strict'
 
 module.exports = (baseUrl, extraHeaders, credentials) => {
-  const { fetch } = global.window ? (window.fetch ? window.fetch : require('whatwg-fetch')) : require('node-fetch')
+  const { fetch } = global.window ? (window.fetch ? window.fetch : require('whatwg-fetch')) : require('node-fetch') // eslint-disable-line node/no-unpublished-require
 
   const req = async (url, method = 'GET', headers = {}, body) => {
     if (body) {
--- src/render/stub-db.js
+++ src/render/stub-db.js
@@ -21,24 +21,6 @@ normalized layout:
 */
 
 module.exports = models => {
-  const mModels = iterateKeysToArstr(models, (modelName, model) => {
-    const schema = L(`new mongoose.Schema(${S(Object.assign(iterateKeys(model.attributes, (attrName, attr) => {
-      if (attr.isNativeType) {
-        return attr.typeObj.mongoose.literalParameters(attr.typeParameters) // TODO: add .typeParameters
-      }
-
-      if (attr.isList) {
-        if (attr.isNativeType) {
-          return L(`{ type: Array, item: ${S(attr.typeObj.mongoose.literalParameters(attr.typeParameters))} }`)
-        }
-
-        return L('{ type: Array, item: mongoose.ObjectId }') // TODO: db relations
-      }
-    }) /* TODO: ACLs */, { parent: { model: L('{ type: String }'), id: L('{ type: mongoose.ObjectId }') } }))})`)
-
-    return L(`mongoose.model(${S(modelName)}, ${S(schema)})`)
-  })
-
   return L(`'use strict'
 
 const ABS = require('celarium/src/abstract/stub-db')
