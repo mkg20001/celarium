cdefaf13ce9f56a6b2aad0379245bb9428a31f83
Maciej Krüger <mkg20001@gmail.com>
2020-09-03 17:30:06 +0200

    feat: more db tests

--- test/db.js
+++ test/db.js
@@ -4,6 +4,7 @@
 /* eslint-disable require-atomic-updates */
 
 const chai = require('chai')
+chai.use(require('chai-as-promised'))
 const expect = chai.expect
 
 const parse = require('../src/acl').parse
@@ -38,6 +39,28 @@ describe('db', () => {
       el = await stubDb.db.getById('board', el.id)
 
       expect(el).to.have.property('name').that.is.equal('Test')
+    })
+
+    it('board update', async () => {
+      await stubDb.db.setById('board', el.id, { name: 'TheTest' })
+
+      el = await stubDb.db.getById('board', el.id)
+
+      expect(el).to.have.property('name').that.is.equal('TheTest')
+    })
+
+    it('board soft delete', async () => {
+      await stubDb.db.delById('board', el.id)
+
+      el = await stubDb.db.getById('board', el.id)
+
+      expect(el).to.have.property('name').that.is.equal('TheTest')
+    })
+
+    it('board hard delete', async () => {
+      await stubDb.db.delById('board', el.id, null, false)
+
+      await expect(stubDb.db.getById('board', el.id)).to.be.rejectedWith(/does not exist/)
     })
 
     after(async () => {
